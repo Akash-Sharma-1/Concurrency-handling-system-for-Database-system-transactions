4f1e3a63447ba8f07311ea0008c2993128eb2b44
Akash Sharma <33986563+Akash-Sharma-1@users.noreply.github.com>
2020-07-16 00:14:48 +0530

    Add files via upload

---
+++ src/database.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class database {
+
+    ArrayList<flight> db;
+    ArrayList<passenger> plist;
+
+    database(ArrayList<flight> a,ArrayList<passenger>b)
+    {
+        this.db=a;
+        this.plist=b;
+    }
+
+
+}
